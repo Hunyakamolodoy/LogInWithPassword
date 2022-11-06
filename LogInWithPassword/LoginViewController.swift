f1de0ab0e43b720260a5fedbc18eaf1fe9313f7a
Hunyakamolodoy <dmitriy_seleznev_01@mail.ru>
2022-11-06 13:44:04 +0700

    Working project

---
+++ LogInWithPassword/LoginViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  LogInWithPassword
+//
+//  Created by Дмитрий Селезнев on 04.11.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet var userTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    let user = "Useruseruser"
+    let password = "qwerty"
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let newTF = segue.destination as? WelcomeViewController else {return}
+        newTF.usernameWelcome = userTF.text
+    }
+    
+    @IBAction func userButtonAction() {
+        showAlert(
+            withTitile: "Oops!",
+            andMessage: "Your name is \(user)"
+        )
+    }
+    
+    @IBAction func passwordButtonAction() {
+        showAlert(
+            withTitile: "Oops!",
+            andMessage: "Your password is \(password)"
+        )
+    }
+    
+    @IBAction func loginButtonAction() {
+        if userTF.text == user && passwordTF.text == password {
+            //check = true
+        } else {
+            showAlert(
+                withTitile: "Something went wrong",
+                andMessage: "Please check your username and password"
+            )
+        }
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard segue.source is WelcomeViewController else {return}
+        userTF.text = ""
+        passwordTF.text = ""
+    }
+}
+
+
+// MARK: - UIAlertController
+
+extension LoginViewController {
+    private func showAlert(withTitile title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
---
+++ LogInWithPassword/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  LogInWithPassword
+//
+//  Created by Дмитрий Селезнев on 06.11.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var usernameWelcome: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        welcomeLabel.text = "Welcome to new VC, \(usernameWelcome ?? "")"
+    }
+}
